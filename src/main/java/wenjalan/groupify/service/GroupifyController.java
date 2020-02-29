3933ee2148183786136c6395d7c0ca49f8836ddd
Alan Wen <alanjameswen@outlook.com>
2020-02-28 22:41:42 -0800

    Controller Cleanup
    Removed the Action enum in favor of having explicit methods for each endpoint (PURGE still unimplemented)

--- src/main/java/wenjalan/groupify/service/GroupifyController.java
+++ src/main/java/wenjalan/groupify/service/GroupifyController.java
@@ -6,108 +6,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import wenjalan.groupify.service.model.Party;
 import wenjalan.groupify.service.model.GroupifyUser;
-import wenjalan.groupify.service.model.webmodel.AddUserResponseModel;
-import wenjalan.groupify.service.model.webmodel.CreatePartyResponseModel;
-import wenjalan.groupify.service.model.webmodel.PartyWebModel;
-import wenjalan.groupify.service.model.webmodel.PlaylistCreatedResponseModel;
+import wenjalan.groupify.service.model.webmodel.*;
 
 import java.net.URI;
 
 // GroupifyController handles the HTTP interfacing of the GroupifyService, including authentication callback
 @RestController
 public class GroupifyController {
-
-    // the possible actions a client can ask of the service
-    public enum Action {
-
-        // create a new party
-        CREATE {
-            // returns: an authorization URI for the user to click on
-            @Override
-            String run(Party party) {
-                GroupifyService service = GroupifyService.getInstance();
-                URI uri = service.createParty();
-                return uri.toString();
-            }
-        },
-
-        // add a guest to the party
-        ADD {
-            @Override
-            String run(Party party) {
-                GroupifyService service = GroupifyService.getInstance();
-                URI uri = service.addUserToParty(party);
-                return uri.toString();
-            }
-        },
-
-        // clear all guests from the party
-        CLEAR {
-            @Override
-            String run(Party party) {
-                GroupifyService service = GroupifyService.getInstance();
-                service.clearParty(party);
-                return "success";
-            }
-        },
-
-        // get information about a user in the party
-        INFO {
-            @Override
-            String run(Party party) {
-                // string together each user's information
-                StringBuilder sb = new StringBuilder();
-                for (GroupifyUser user : party.getUsers()) {
-                    sb.append(user.getInfo() + "\n");
-                }
-                return sb.toString();
-            }
-        },
-
-        // makes the playlist on the host's account
-        MAKE {
-            @Override
-            String run(Party party) {
-                return "" + GroupifyService.getInstance().makePlaylist(party);
-            }
-        },
-
-        // purge Groupify playlists from the host's account
-        PURGE {
-            @Override
-            String run(Party party) {
-                GroupifyService service = GroupifyService.getInstance();
-                // get the host of this party
-                GroupifyUser host = party.getHost();
-                // purge their playlists
-                boolean success = service.purgePlaylists(host);
-                // return success
-                if (success) {
-                    return "success";
-                }
-                else {
-                    return "an error occurred";
-                }
-            }
-        },
-
-        // stops the session with the API
-        STOP {
-            @Override
-            String run(Party party) {
-                // get the party manager
-                PartyManager manager = PartyManager.getInstance();
-                // deregister this party
-                manager.unregister(party);
-                // return success
-                return "success";
-            }
-        };
-
-        // methods
-        abstract String run(Party party);
-
-    }
 
     // receives a Spotify Authentication Callback
     @RequestMapping(value = "api/callback")
@@ -138,61 +43,6 @@ public class GroupifyController {
         return "error retrieving party with id " + partyId;
     }
 
-    // receives an Action request
-    @CrossOrigin
-    @RequestMapping(value = "api/action")
-    public String action(
-            @RequestParam(value = "action", defaultValue = "") String action,
-            @RequestParam(value = "party", defaultValue = "") String party) {
-        // if no action was specified, return an error
-        if (action.isEmpty()) {
-            return "no action specified, please specify an action";
-        }
-
-        // find which action was requested
-        for (Action a : Action.values()) {
-            // if the action requested matches a command
-            if (action.equalsIgnoreCase(a.name())) {
-                // run it and return its response
-                Party p = getParty(party);
-
-                // if no party was found and the action wasn't a create
-                if (p == null && !action.equalsIgnoreCase("create")) {
-                    // return an error
-                    return "no party specified, please specify a party";
-                }
-
-                return a.run(p);
-            }
-        }
-
-        // if none was found, return an error
-        return "unrecognized action: " + action;
-    }
-
-    // receives a Join party request
-    @CrossOrigin
-    @RequestMapping(value = "api/join")
-    public String join(
-            @RequestParam(value = "party", defaultValue = "") String party) {
-        // if no party specified, complain
-        if (party.isEmpty()) {
-            return "please specify a party id";
-        }
-
-        // find the party
-        Party p = getParty(party);
-
-        // if none found, complain
-        if (p == null) {
-            return "no party with id " + party + " found";
-        }
-
-        // otherwise, return an invite link
-        String url = Action.ADD.run(p);
-        return url;
-    }
-
     // returns a PartyWebModel JSON given an id
     @CrossOrigin
     @RequestMapping(value = "api/party")
@@ -212,33 +62,65 @@ public class GroupifyController {
     @CrossOrigin
     @RequestMapping(value = "api/create")
     public CreatePartyResponseModel create() {
-        return new CreatePartyResponseModel(Action.CREATE.run(null));
+        // get an auth uri for a new party
+        GroupifyService g = GroupifyService.getInstance();
+        URI uri = g.createParty();
+        return new CreatePartyResponseModel(uri.toString());
     }
 
     // adds a user to a party
     @CrossOrigin
     @RequestMapping(value = "api/add")
     public AddUserResponseModel add(@RequestParam(value = "party", defaultValue = "") String partyId) {
-        Party p = getParty(partyId);
-        if (p == null) {
-            throw new IllegalArgumentException("no party with id " + partyId + " found");
-        }
-        return new AddUserResponseModel(Action.ADD.run(p));
-    }
-
-    // makes the playlist given a party
-    @CrossOrigin
-    @RequestMapping(value = "api/make")
-    public PlaylistCreatedResponseModel make(
-            @RequestParam(value = "party", defaultValue = "") String partyId){
         // get party
         Party p = getParty(partyId);
         if (p == null) {
             throw new IllegalArgumentException("no party with id " + partyId + " found");
         }
 
+        // generate an auth URI for the user for this party
+        GroupifyService g = GroupifyService.getInstance();
+        URI uri = g.addUserToParty(p);
+
+        // generate an auth URI for the user from this party
+        return new AddUserResponseModel(uri.toString());
+    }
+
+    // makes the playlist given a party
+    @CrossOrigin
+    @RequestMapping(value = "api/make")
+    public PlaylistCreatedResponseModel make(
+            @RequestParam(value = "party", defaultValue = "") String partyId,
+            @RequestParam(value = "addRecommendations", defaultValue = "") String addRecommendations){
+        // get party
+        Party p = getParty(partyId);
+        if (p == null) {
+            throw new IllegalArgumentException("no party with id " + partyId + " found");
+        }
+
+        // make the playlist
+        GroupifyService g = GroupifyService.getInstance();
+        String url = g.makePlaylist(p);
+
         // return JSON response
-        return new PlaylistCreatedResponseModel(Action.MAKE.run(p));
+        return new PlaylistCreatedResponseModel(url);
+    }
+
+    // delists the party from the service
+    @CrossOrigin
+    @RequestMapping(value = "api/remove")
+    public PartyRemoveResponseModel remove(
+            @RequestParam(value = "party", defaultValue = "") String partyId) {
+        // get party
+        Party p = getParty(partyId);
+        if (p == null) {
+            throw new IllegalArgumentException("no party with id " + partyId + " found");
+        }
+
+        // remove the party from the manager
+        PartyManager pm = PartyManager.getInstance();
+        pm.unregister(p);
+        return new PartyRemoveResponseModel("success");
     }
 
     // returns a party given a String id
---
+++ src/main/java/wenjalan/groupify/service/model/webmodel/PartyRemoveResponseModel.java
@@ -0,0 +1,11 @@
+package wenjalan.groupify.service.model.webmodel;
+
+public class PartyRemoveResponseModel extends WebModel {
+
+    String message;
+
+    public PartyRemoveResponseModel(String message) {
+        this.message = message;
+    }
+
+}
